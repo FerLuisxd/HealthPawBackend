9feeec99d2b38d87cfc30ace4706ad4344a3ef17
Luis Vilca Flores <ferluisxd@gmail.com>
2020-10-09 01:19:32 -0500

    feat( cronjob)

--- src/pet/pet.service.ts
+++ src/pet/pet.service.ts
@@ -3,6 +3,8 @@ import * as AWS from 'aws-sdk'
 import { Pet, Stadistics } from './pet.entity';
 import { v4 as uuidv4 } from 'uuid';
 import * as moment from 'moment'
+import { Cron } from '@nestjs/schedule';
+
 @Injectable()
 export class PetService {
     ddb: AWS.DynamoDB.DocumentClient;
@@ -116,6 +118,36 @@ export class PetService {
         await this.ddb.update(params).promise();
         return { "message": "sucess" };
     }
+
+    modifyArray(stadistic, today){
+        stadistic.todayHistory = stadistic.todayHistory.filter(function(x) { 
+            return moment.unix(x.timestamp).set({ minute: 0, second: 0, millisecond: 0, hour: 0 }).unix() == today.unix()
+        })
+        let newDay = today.unix() != moment.unix(stadistic.todayHistory[0].timestamp).set({ minute: 0, second: 0, millisecond: 0, hour: 0 }).unix()
+       
+        let min = stadistic.todayHistory[0].value || 0
+        let max = stadistic.todayHistory[0].value || 0
+        let average = 0
+        for (let i = 0; i < stadistic.todayHistory.length; i++) {
+            if (min > stadistic.todayHistory[i].value)
+                min = stadistic.todayHistory[i].value
+            if (max < stadistic.todayHistory[i].value)
+                max = stadistic.todayHistory[i].value
+            average += stadistic.todayHistory[i].value
+        }
+        average = average / stadistic.todayHistory.length
+        if(!newDay) {
+            //Add to todayHistory
+            let historyIndex = stadistic.history.findIndex(x => moment.unix(x.timestamp).set({ minute: 0, second: 0, millisecond: 0, hour: 0 }).unix() == today.unix())
+            stadistic.history[historyIndex].value = average
+        }
+        stadistic.minimum = min
+        stadistic.maximum = max
+        stadistic.average = average
+
+        return stadistic
+    }
+
     async updatePetStadistic(id: string, body: Stadistics): Promise<any> {
         const res = await this.ddb.get({ TableName: this.tableName, Key: { 'id': id } }).promise()
         if (res.Item) {
@@ -338,4 +370,26 @@ export class PetService {
         await this.ddb.delete(params).promise();
         return { "message": "sucess" };
     }
+
+    @Cron('0 0 0 * * *', {'timeZone':'America/Lima'})
+    async cronJobHours(){
+        const today = moment().set({ minute: 0, second: 0, millisecond: 0, hour: 0 })
+        let pets = await this.getPets()
+        for (let i = 0; i < pets.length; i++) {
+            let current = pets[i]
+            let obj = new Pet()
+            current.breathingFrequency = this.modifyArray(current.breathingFrequency,today )
+            obj.breathingFrequency = current.breathingFrequency
+            current.temperature = this.modifyArray(current.temperature,today )
+            obj.temperature = current.temperature
+            current.sound = this.modifyArray(current.sound,today )
+            obj.sound = current.sound
+            current.heartRate = this.modifyArray(current.heartRate,today )
+            obj.heartRate = current.heartRate
+
+            await this.updatePet(current.id,obj)
+            
+        }
+    }
+
 }
